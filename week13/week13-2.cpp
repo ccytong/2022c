7926e6f78d94eabe1b5245ddc43f08328ce2d90d
ccytong <wendy9502262@gmail.com>
2022-12-07 14:50:38 +0800

    week13

--- week13/week13-2.cpp
+++ week13/week13-2.cpp
@@ -2,8 +2,8 @@
 int a[50];
 int main()
 {
-    a[0] = 0;
-    a[1] = 1;
+    a[0]=0;
+    a[1]=1;
     for(int i=2; i<45; i++){
         a[i]=a[i-1]+a[i-2];
     }
@@ -11,5 +11,3 @@ int main()
         printf("%d ", a[i]);
     }
 }
-
-
